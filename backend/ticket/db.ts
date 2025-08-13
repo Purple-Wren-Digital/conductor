45790799d62ba3ad259124b94caeb09628ce5453
Victoria McNorrill <vmcnorrill@gmail.com>
2025-08-13 13:35:22 -0400

    reconfig ticket/db.ts

--- backend/ticket/db.ts
+++ backend/ticket/db.ts
@@ -1,14 +1,8 @@
 import { SQLDatabase } from "encore.dev/storage/sqldb";
 import { PrismaClient } from "@prisma/client";
 
-export const ticketDB = new SQLDatabase("ticket", {
-  migrations: "./migrations",
-});
+export const ticketDB = new SQLDatabase("ticket"); // ← remove migrations
 
 export const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: ticketDB.connectionString,
-    },
-  },
+  datasources: { db: { url: ticketDB.connectionString } },
 });
