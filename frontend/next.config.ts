7ddb5bbf46f2aa217dee1b47b7fddd75c9ce128f
Garth Groshans Jr <garthgroshansjr@gmail.com>
2025-08-22 09:20:44 -0400

    fe config (for encore proxy) and types (need to be moved)

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+    async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://127.0.0.1:4000/:path*', // Proxy to Encore
+      },
+    ]
+  },
 };
 
 export default nextConfig;
