61a9dd7b3ea8b95d907891669dc49653d4da085f
Caleb McQuaid <calebmcquaid@gmail.com>
2025-10-24 14:09:59 -0400

    fix backend migrations

--- backend/ticket/db.ts
+++ backend/ticket/db.ts
@@ -1,10 +1,12 @@
 import { SQLDatabase } from "encore.dev/storage/sqldb";
 import { PrismaClient } from "@prisma/client";
-import { ensurePrismaSchema } from "./ensurePrisma";
 
-export const ticketDB = new SQLDatabase("ticket");
-
-export const ready = ensurePrismaSchema(); // ensure Prisma schema applied in preview
+export const ticketDB = new SQLDatabase("ticket", {
+  migrations: {
+    path: "./migrations",
+    source: "prisma",
+  },
+});
 
 export const prisma = new PrismaClient({
   datasources: { db: { url: ticketDB.connectionString } },
