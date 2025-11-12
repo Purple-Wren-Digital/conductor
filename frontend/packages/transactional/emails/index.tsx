39bf583b6ba87cc87729a87e9846d86eb6afa6b6
Victoria McNorrill <vmcnorrill@gmail.com>
2025-11-12 13:18:10 -0500

    vercel build error - quick edit notification removed

--- frontend/packages/transactional/emails/index.tsx
+++ frontend/packages/transactional/emails/index.tsx
@@ -1,6 +1,5 @@
 export { default as CreatedTicketNotification } from "./CreatedTicketNotification";
 export { default as MarketCenterUserUpdate } from "./MarketCenterUserUpdate";
 export { default as NewCommentNotification } from "./NewCommentNotification";
-export { default as QuickEditTicketNotification } from "./QuickEditTicketNotification";
 export { default as TicketAssignment } from "./TicketAssignment";
 export { default as UserInvitation } from "./UserInvitation";
