1456b836b5d3e38a93990b818072ce1ef0551aa3
Victoria McNorrill <vmcnorrill@gmail.com>
2025-11-14 16:01:57 -0500

    build error - params async/await

--- frontend/app/dashboard/marketCenters/[id]/team/page.tsx
+++ frontend/app/dashboard/marketCenters/[id]/team/page.tsx
@@ -1,11 +1,11 @@
 import MarketCenterAgentView from "@/components/ui/marketCenters/market-center-agent-view";
 
-export default function MarketCenterTeamPage({
+export default async function MarketCenterTeamPage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
-  const { id } = params;
+  const { id } = await params;
 
   return <MarketCenterAgentView marketCenterId={id} />;
 }
