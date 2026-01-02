1275302a081bd014b08f0a8999ea0bd3894dfaaf
Victoria McNorrill <vmcnorrill@gmail.com>
2026-01-02 16:18:43 -0500

    create default mc notification preferences if none found

--- backend/marketCenters/notification-preferences/get.ts
+++ backend/marketCenters/notification-preferences/get.ts
@@ -2,6 +2,7 @@ import { api, APIError } from "encore.dev/api";
 import { getUserContext } from "../../auth/user-context";
 import { marketCenterRepository } from "../../shared/repositories/market-center.repository";
 import type { MarketCenterNotificationPreferences } from "../../settings/types";
+import { defaultMarketCenterNotificationPreferences } from "./utils";
 
 export interface GetMarketCenterNotificationPreferencesRequest {
   marketCenterId: string;
@@ -33,11 +34,36 @@ export const getMCNotificationPreferences = api<
     const mc = await marketCenterRepository.findById(req.marketCenterId);
     if (!mc) throw new Error("Market center not found");
 
-    const notificationPreferences: MarketCenterNotificationPreferences[] =
-      mc?.settings?.notificationPreferences &&
+    let notificationPreferences: MarketCenterNotificationPreferences[] = [];
+
+    if (
+      mc.settings &&
+      mc.settings.notificationPreferences &&
       mc?.settings?.notificationPreferences.length > 0
-        ? mc.settings.notificationPreferences
-        : [];
+    ) {
+      notificationPreferences = mc.settings.notificationPreferences;
+    } else {
+      const marketCenterUpdated = await marketCenterRepository.update(
+        req.marketCenterId,
+        {
+          settings: {
+            ...mc.settings,
+            notificationPreferences: defaultMarketCenterNotificationPreferences,
+          },
+        }
+      );
+      if (
+        marketCenterUpdated?.settings &&
+        marketCenterUpdated?.settings?.notificationPreferences &&
+        marketCenterUpdated.settings.notificationPreferences.length > 0
+      ) {
+        notificationPreferences =
+          marketCenterUpdated.settings.notificationPreferences;
+      }
+    }
+    if (!notificationPreferences || notificationPreferences.length === 0) {
+      throw new Error("Notification preferences not found");
+    }
 
     return { notificationPreferences: notificationPreferences };
   }
