c9be9cb744f8c608a403186eda52589114191742
Caleb McQuaid <calebmcquaid@gmail.com>
2026-04-27 11:07:02 -0400

    Clerk version update

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -8,19 +8,22 @@ const isPublicRoute = createRouteMatcher([
   "/pricing",
 ]);
 
-export default clerkMiddleware(async (auth, request) => {
-  if (!isPublicRoute(request)) {
-    const authObject = await auth();
+export default clerkMiddleware(
+  async (auth, request) => {
+    if (!isPublicRoute(request)) {
+      const authObject = await auth();
 
-    if (!authObject.userId) {
-      await auth.protect();
+      if (!authObject.userId) {
+        await auth.protect();
+      }
     }
-  }
-});
+  },
+  { frontendApiProxy: { enabled: true } }
+);
 
 export const config = {
   matcher: [
-    "/((?!_next|__clerk|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc|__clerk)(.*)",
   ],
 };
