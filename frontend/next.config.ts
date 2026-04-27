05f95a1fb69a144f762b9b697ad50a1fb120ae45
Caleb McQuaid <calebmcquaid@gmail.com>
2026-04-27 10:53:38 -0400

    move proxy to vercel

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -13,10 +13,6 @@ const nextConfig: NextConfig = {
         source: "/api/:path*",
         destination: "http://127.0.0.1:4000/:path*", // Proxy to Encore
       },
-      {
-        source: "/__clerk/:path*",
-        destination: "https://clerk.conductortickets.com/:path*", // Proxy Clerk Frontend API
-      },
     ];
   },
 };
