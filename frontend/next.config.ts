8b64c59e409092bc7bd9c32bcff281b3be3e692f
Caleb McQuaid <calebmcquaid@gmail.com>
2026-04-27 10:43:09 -0400

    Clerk proxy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -9,9 +9,13 @@ const nextConfig: NextConfig = {
 
   async rewrites() {
     return [
-{
+      {
         source: "/api/:path*",
         destination: "http://127.0.0.1:4000/:path*", // Proxy to Encore
+      },
+      {
+        source: "/__clerk/:path*",
+        destination: "https://clerk.conductortickets.com/:path*", // Proxy Clerk Frontend API
       },
     ];
   },
